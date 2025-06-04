b3116bb7cce97e5b515e998cf0a6319d5aad9c7f
MarceloEscobal <marceloescobal11@gmail.com>
2025-06-04 02:27:20 -0500

    cambios

--- src/main/java/upao/edu/pe/config/CorsConfig.java
+++ src/main/java/upao/edu/pe/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // ajusta según tu ruta base
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:4200", "https://clasificador-sunat.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
